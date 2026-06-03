39dfbca26f3dcc1407a81781d0b517b6f1c53bf4
sanskargadhave <sanskargadhave024@gmail.com>
2026-06-03 23:59:57 +0530

    odify

--- backend/Routes/StudentRoutes.js
+++ backend/Routes/StudentRoutes.js
@@ -1,6 +1,6 @@
 const express =require("express");
 const router=express.Router();
-const {getMentordetails,sendApplication,getapplication,getProfiledetails,updateProfiledetails}=require("../controllers/StudentControler");
+const {getMentordetails,sendApplication,getapplication}=require("../controllers/StudentControler");
 const verifyToken=require("../middleware/authmiddleware");
 const isStudent=require("../middleware/isstudentmiddleware");
 
@@ -8,11 +8,7 @@ router.use(verifyToken,isStudent);
 
 router.get("/get-mentordetails/:studentid",getMentordetails);
 router.get("/get-student-applications/:id",getapplication);
-router.get("/get-profiledetails/:id",getProfiledetails);
 
-
-
-router.put("/updateprofile-details",updateProfiledetails);
 
 
 router.post("/send-application",sendApplication);
