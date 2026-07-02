19f9daed37c7d6174a8f5367a96dac48c64ec34b
sanskargadhave <sanskargadhave024@gmail.com>
2026-07-02 18:43:34 +0530

    modify

--- backend/controllers/notificationControler.js
+++ backend/controllers/notificationControler.js
@@ -3,8 +3,10 @@ const NotificationSchema = require("../model/notificationsScema");
 const getNotification = async (req, resp) => {
   try {
     const { id } = req.params; 
-   console.log("Notification route hit");
+   
+
     const notifications = await NotificationSchema.find({receiverid: id}).sort({ createdAt: -1 }); 
+    console.log(notifications);
     resp.status(200).json(notifications);
   } catch (err) {
     resp.status(500).json({ message: err.message || "Error fetching notifications" });
