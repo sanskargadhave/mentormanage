f72b9f872c7d2b89738c423177f07192e4e5a2dc
sanskargadhave <sanskargadhave024@gmail.com>
2026-07-16 11:31:46 +0530

    modify

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,18 +1,29 @@
 const nodemailer = require("nodemailer");
 const {OtpTemplate}=require("../templates/OtpTemplate");
-
+console.log(process.env.BREVO_SMTP_LOGIN)
+console.log(process.env.BREVO_SMTP_KEY)
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.BREVO_SMTP_LOGIN,
         pass: process.env.BREVO_SMTP_KEY,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 const sendOTP = async (email, otp) => {
-    
+    transporter.verify((err, success) => {
+    if (err) {
+        console.error("SMTP Verify Error:", err);
+    } else {
+        console.log("SMTP Ready");
+    }
+});
     await transporter.sendMail({
 
         from: `"EduMentor" <${process.env.EMAIL_USER}>`,
